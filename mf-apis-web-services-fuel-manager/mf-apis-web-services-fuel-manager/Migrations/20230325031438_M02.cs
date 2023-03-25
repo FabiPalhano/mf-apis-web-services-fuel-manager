1fcb504fb9eee30c47edce9e98fd0ff153c0bd30
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2023-03-25 20:09:15 -0300

    definido controller do usuário

---
+++ mf-apis-web-services-fuel-manager/mf-apis-web-services-fuel-manager/Migrations/20230325031438_M02.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mf_apis_web_services_fuel_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class M02 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuários",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Perfil = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuários", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Veículo Usuários",
+                columns: table => new
+                {
+                    VeiculoId = table.Column<int>(type: "int", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Veículo Usuários", x => new { x.VeiculoId, x.UsuarioId });
+                    table.ForeignKey(
+                        name: "FK_Veículo Usuários_Usuários_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuários",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Veículo Usuários_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Veículo Usuários_UsuarioId",
+                table: "Veículo Usuários",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Veículo Usuários");
+
+            migrationBuilder.DropTable(
+                name: "Usuários");
+        }
+    }
+}
