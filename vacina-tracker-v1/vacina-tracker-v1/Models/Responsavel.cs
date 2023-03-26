64038343620eb7b699fab143568467abd77ac4ff
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2023-03-26 16:46:23 -0300

    criação e configuração dos models de vacina e responsavel

---
+++ vacina-tracker-v1/vacina-tracker-v1/Models/Responsavel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vacina_tracker_v1.Models
+{
+    [Table("Perfil Usuário Responsável")]
+    public class Responsavel
+    {
+        [Key]
+        public int IdResponsavel { get; set; }
+
+        [Display(Name = "Nome Completo")]
+        [Required(ErrorMessage = ("Obrigatório informar o nome completo"))]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = ("Obrigatório informar o CPF"))]
+        public string CPF { get; set; }
+              
+        [Required(ErrorMessage = ("Obrigatório informar o email"))]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = ("Obrigatório informar senha"))]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = ("Obrigatório repetir a  senha"))]
+        public string Senha2 { get; set; }
+
+        [Display(Name = "Tipo de Usuário")]
+        public TipoUsuario TipoUsuario { get; set; }
+
+        [Required]
+        public int VacinaId { get; set; }
+
+        public Vacina Vacina { get; set; }
+    }
+
+
+    public enum TipoUsuario
+    {
+        [Display(Name = "Responsável")]
+        TipoResponsavel,
+        [Display(Name = "Dependente")]
+        TipoDependente
+
+    }
+}
---
+++ vacina-tracker-v1/vacina-tracker-v1/Models/Vacina.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vacina_tracker_v1.Models
+{
+    [Table("Vacinas")]
+    public class Vacina
+    {
+        [Key]
+        public int IdVacina { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public DateOnly DataAplicacao { get; set; }
+
+        [Required]
+        public NumeroDose NumeroDose { get; set; }
+
+        [Required]
+        public DateOnly DataProximaAplicacao { get; set; }
+
+        public ICollection<Responsavel> Responsavel { get; set; }
+    }
+
+    public enum NumeroDose
+    {
+        [Display(Name = "Dose Única")]
+        DoseUnica,
+        [Display(Name = "Primeira Dose")]
+        PrimeiraDose,
+        [Display(Name = "Segunda Dose")]
+        SegundaDose,
+        [Display(Name = "Terceira Dose")]
+        TerceiraDose
+    }
+}
