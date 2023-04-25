9b407299cddf3617e47b170354fea7f81c475cb9
Fabiana Palhano <fabianabprocha@yahoo.com.br>
2023-04-24 22:52:40 -0300

    criação das models vacina e membro

---
+++ vacina-tracker-v4/vacina-tracker-v4/Models/Membro.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vacina_tracker_v4.Models
+{
+    [Table("Membros")]
+    public class Membro 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = ("Obrigatório informar o Nome"))]
+        [MaxLength(30)]
+        public string NomeMembroFamilia { get; set; }
+
+        [Required(ErrorMessage = ("Obrigatório informar a idade"))]
+        public int Idade { get; set; }
+
+
+        public ICollection<Vacina> Vacinas { get; set; } //1 membro está associado a várias vacinas
+
+        //public ICollection<VacinaMembro> Vacinas { get; set; }
+    }
+}
